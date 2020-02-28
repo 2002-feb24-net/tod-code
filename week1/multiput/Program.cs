dbd432c56ef2a9d5e04244c32254616430edaac9
tshjones <tshjones@yahoo.com>
2020-02-28 09:09:52 -0600

    day 5 program acronym

---
+++ week1/multiput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace multiput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Phrase:");
+            string phrase = Console.ReadLine();
+            Console.Write(phrase);
+            
+        }
+    }
+}
