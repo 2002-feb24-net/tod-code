25e0509ea24c6eb654ae044673ed556e17574a24
tshjones <tshjones@yahoo.com>
2020-02-28 09:20:06 -0600

    Acronym program implemented

--- week1/multiput/Program.cs
+++ week1/multiput/Program.cs
@@ -8,8 +8,22 @@ namespace multiput
         {
             Console.Write("Enter Phrase:");
             string phrase = Console.ReadLine();
-            Console.Write(phrase);
+            string acronym = Acronym(phrase);
+            Console.Write(acronym);
             
+        }
+
+        static string Acronym(string phrase)
+        {
+            string acronym = "";
+            string word;
+            string[] words = phrase.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                word = words[i];
+                acronym += word[0];
+            }
+            return acronym;
         }
     }
 }
