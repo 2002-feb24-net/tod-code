260e1842f42be331024c0b989bd4f0c8aeb0cba8
tshjones <tshjones@yahoo.com>
2020-02-28 09:29:58 -0600

    made acronym uppercase and added comments

--- week1/multiput/Program.cs
+++ week1/multiput/Program.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿/*
+Tod Jones
+This program takes a multiline input
+and outputs the acronym in uppercase to console
+
+*/
+using System;
 
 namespace multiput
 {
     class Program
     {
+        // main does basic IO 
         static void Main(string[] args)
         {
             Console.Write("Enter Phrase:");
@@ -12,7 +19,8 @@ namespace multiput
             Console.Write(acronym);
             
         }
-
+//Acronym splits phrase into string array of words, then 
+//returns new string with first element of each word in uppercase
         static string Acronym(string phrase)
         {
             string acronym = "";
@@ -23,7 +31,7 @@ namespace multiput
                 word = words[i];
                 acronym += word[0];
             }
-            return acronym;
+            return acronym.ToUpper();
         }
     }
 }
