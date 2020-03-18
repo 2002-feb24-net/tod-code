5891dd0c1175f24bd76d5e2654e1c1bb9902ed7f
tshjones <tshjones@yahoo.com>
2020-03-18 14:38:13 -0500

    database string

---
+++ week2/database/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Server=tcp:training-tshjones.database.windows.net,1433;Initial Catalog=training;Persist Security Info=False;User ID=Gwyrddu;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+        }
+    }
+}
